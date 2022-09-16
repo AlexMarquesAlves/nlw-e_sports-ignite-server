65bbfa9472cee88fbe588c8b4444616560c8bb11
¡Duque <atanaelleonardo@gmail.com>
2022-09-16 20:52:30 -0300

    🆕 /games/:id/ads Route added

--- src/server.ts
+++ src/server.ts
@@ -18,5 +18,16 @@ app.post('/ads', (req, res) => {
   return res.status(201).json([]);
 });
 
+app.get('/games/:id/ads', (req, res) => {
+  // const gameId = req.params.id;
+
+  return res.json([
+    { id: 1, name: 'Anuncio 1' },
+    { id: 2, name: 'Anuncio 2' },
+    { id: 3, name: 'Anuncio 3' },
+    { id: 4, name: 'Anuncio 4' },
+  ]);
+});
+
 // Server listen
 app.listen(3333);
