5fd665286bb0bf7b033a8ad9fd47c1dccd1dcbb9
¡Duque <atanaelleonardo@gmail.com>
2022-09-16 20:53:04 -0300

    🆕 /ads/:id/discord Route added

--- src/server.ts
+++ src/server.ts
@@ -29,5 +29,16 @@ app.get('/games/:id/ads', (req, res) => {
   ]);
 });
 
+app.get('/ads/:id/discord', (req, res) => {
+  // const adId = req.params.id;
+
+  return res.json([
+    { id: 1, name: 'Anuncio 1' },
+    { id: 2, name: 'Anuncio 2' },
+    { id: 3, name: 'Anuncio 3' },
+    { id: 4, name: 'Anuncio 4' },
+  ]);
+});
+
 // Server listen
 app.listen(3333);
