ed10edeae076556e86585d1f5baaf108373d3e2c
¡Duque <atanaelleonardo@gmail.com>
2022-09-17 01:46:08 -0300

    🆕 convertMinutesToHourString function created

---
+++ src/utils/convert-minutes-to-hour-string.ts
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param minutesAmount 1100 --> 18:20
+ */
+
+export function convertMinutesToHourString(minutesAmount: number) {
+  const hours = Math.floor(minutesAmount / 60);
+  const minutes = minutesAmount % 60;
+
+  return `${hours}:${minutes}`;
+}
